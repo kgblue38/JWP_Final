8867cea68d800f18c1abd3b664805b6df54b94ae
kgblue38 <kgblue38@naver.com>
2016-05-16 11:09:02 +0900

    scaffolding project

---
+++ src/main/java/org/next/JwpFinalApplication.java
@@ -0,0 +1,12 @@
+package org.next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpFinalApplication.class, args);
+	}
+}
